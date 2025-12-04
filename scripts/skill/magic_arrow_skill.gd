bde444c5d5823c345ddf5991a4ab7761eb73a864
Hatedero <amaurymenneret@outlook.fr>
2025-12-04 22:27:12 +0100

    push

--- scripts/skill/magic_arrow_skill.gd
+++ scripts/skill/magic_arrow_skill.gd
@@ -7,7 +7,7 @@ extends Skill
 	
 func cast(source : LivingEntity, target : LivingEntity) -> void:
 	for x in count:
-		summon_magic_arrow(source.position + Vector3(x*0.1,0,0), ( target.position - source.position).normalized())
+		summon_magic_arrow(source.position + Vector3(x*0.2,0,0), ( target.position - source.position).normalized())
 
 func summon_magic_arrow(source : Vector3, direction : Vector3) -> void:
 	var instance = magic_arrow_scene.instantiate() as MagicArrow
