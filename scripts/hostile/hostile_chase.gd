e80b06ea3cfd794a43cb2a9918460c1f878a6e7d
Hatedero <amaurymenneret@outlook.fr>
2025-12-04 14:13:50 +0100

    basic enemy sprite

--- scripts/hostile/hostile_chase.gd
+++ scripts/hostile/hostile_chase.gd
@@ -9,6 +9,8 @@ var direction : Vector3
 @onready var player : Player
 
 func enter():
+	if hostile is HostileNormal:
+		hostile.sprite.play("walk")
 	player = get_tree().get_first_node_in_group("Player")
 	pathtrace()
 
@@ -16,6 +18,8 @@ func pathtrace() :
 	direction = player.position - hostile.position
 	direction = direction.normalized()
 	hostile.velocity = direction * speed
+	if hostile is HostileNormal:
+		hostile.sprite.flip_h = direction.x < 0
 
 func Physics_Update(delta: float):
 	if Engine.get_frames_drawn() % 2 == hostile.id % 2:
--- scripts/hostile/hostile_normal.gd
+++ scripts/hostile/hostile_normal.gd
@@ -1,3 +1,5 @@
 class_name HostileNormal
 
 extends Hostile
+
+@export var sprite : AnimatedSprite3D
