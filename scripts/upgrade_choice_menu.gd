a1adb80406ddf697a45e60d4e8fa9069fc460c50
Hatedero <amaurymenneret@outlook.fr>
2025-12-18 12:07:06 +0100

    only different upgrades in selection

--- scripts/upgrade_choice_menu.gd
+++ scripts/upgrade_choice_menu.gd
@@ -4,7 +4,7 @@ extends Control
 
 signal choosen(upgrade: PackedScene)
 
-@onready var upgrades : Array[Upgrade] = []
+@onready var upgrades : Dictionary = {}
 @export var upgrade_choice_scene : PackedScene
 
 @export var possible_upgrades : Array[String] = [
@@ -15,32 +15,37 @@ signal choosen(upgrade: PackedScene)
 ]
 
 func add_upgrade_choice(upgrade_choice : Upgrade) -> void :
-	if !upgrades.has(upgrade_choice):
-		upgrades.append(upgrade_choice)
+	if !upgrades.has(upgrade_choice.title):
+		upgrades.set(upgrade_choice.title, upgrade_choice)
 		add_child(upgrade_choice)
 	
 func draw() -> void :
-	for choice in upgrades:
-		#print(choice)
+	var index = 1
+	for choice in upgrades.values():
+		print(choice)
 		var up_choice : UpgradeChoice = upgrade_choice_scene.instantiate()
 		up_choice.upgrade = choice
 		add_child(up_choice)
 		up_choice.scale = Vector2(3,3)
-		up_choice.position = Vector2(((get_viewport().get_visible_rect().size.x/3) * (upgrades.find(choice) + 1) ) - (get_viewport().get_visible_rect().size.x/3)/2 - (up_choice.size.x * 1.5), (get_viewport().get_visible_rect().size.y/2) - (up_choice.size.y * 1.5))
+		up_choice.position = Vector2(((get_viewport().get_visible_rect().size.x/3) * index ) - (get_viewport().get_visible_rect().size.x/3)/2 - (up_choice.size.x * 1.5), (get_viewport().get_visible_rect().size.y/2) - (up_choice.size.y * 1.5))
+		index += 1
 
 func get_random_upgrade() -> Upgrade:
 	return (load(possible_upgrades.pick_random()) as PackedScene).instantiate()
 	
 func get_random_upgrades(nb : int) -> void:
-	var upgrades_to_be_added:Array[Upgrade] = []
+	var upgrades_to_be_added:Dictionary = {}
 	for x in nb:
 		var possible_upgrade = get_random_upgrade()
-		if !upgrades_to_be_added.has(possible_upgrade):
-			upgrades_to_be_added.append(possible_upgrade)
+		if !upgrades_to_be_added.has(possible_upgrade.title):
+			upgrades_to_be_added.set(possible_upgrade.title, possible_upgrade)
 		else :
-			while upgrades_to_be_added.has(possible_upgrade):
+			var index = 1
+			while upgrades_to_be_added.has(possible_upgrade.title):
 				possible_upgrade = get_random_upgrade()
-	for comming_upgrade in upgrades_to_be_added:
+				index += 1
+			upgrades_to_be_added.set(possible_upgrade.title, possible_upgrade)
+	for comming_upgrade in upgrades_to_be_added.values():
 		add_upgrade_choice(comming_upgrade)
 	
 
